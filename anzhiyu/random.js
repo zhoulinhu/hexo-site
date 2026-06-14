58f12497aa2826ecc2ec87c320c5cea301220d58
zhoulinhu <mailbox_of_zlh@163.com>
2026-06-14 14:57:33 +0800

    清空库

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2026/06/06/hello-world/","2026/06/06/测试这是一篇新的博客/","2026/06/11/2026-06-11-多博客搭配文档仓库的搭建/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
